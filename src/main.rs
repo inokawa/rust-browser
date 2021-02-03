d16751c64fe61dcf042c9f667d15cc44ff903007
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-04 01:34:01 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use rust_browser;
+use rust_browser::{css, html, style};
 
 fn main() {
-    println!("Hello, world!");
+    let html_source: String = "foo".to_string();
+    let css_source: String = "bar".to_string();
+    let root_node = html::parse(html_source);
+    let stylesheet = css::parse(css_source);
+    let style_root = style::style_tree(&root_node, &stylesheet);
 }
